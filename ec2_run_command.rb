a1f44f7471d6cc85950d165a42940a2368b57cfc
山田 俊輔 <yshunske@gmail.com>
2019-08-24 17:48:12 +0900

    add:run command test script

---
+++ ec2_run_command.rb
@@ -0,0 +1,17 @@
+require 'aws-sdk'
+
+Aws.config.update(
+  access_key_id:     ENV['SSM_ACCESS_KEY_ID'],
+  secret_access_key: ENV['SSM_SECRET_ACCESS_KEY'],
+  region:            'ap-northeast-1'
+)
+
+Aws::SSM::Client.new.send_command(
+  document_name: "AWS-RunShellScript",
+  instance_ids: [ENV['EC2_INSTANCE_ID']],
+  parameters: {
+    commands: ["echo Hello > /tmp/hello.txt"],
+    executionTimeout:["3600"]
+  },
+  timeout_seconds: 600,
+)
